6adeb778aa8af5153810be8b672764016340ea66
ferrreo <harderthanfire@gmail.com>
2023-06-16 19:05:55 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,10 +1,5 @@
 DEBIAN_FRONTEND=noninteractive
 
-# Add dependent repositories
-wget -q -O - https://ppa.pika-os.com/key.gpg | sudo apt-key add -
-add-apt-repository https://ppa.pika-os.com
-add-apt-repository ppa:pikaos/pika
-add-apt-repository ppa:kubuntu-ppa/backports
 # Clone Upstream
 apt install -y rpm
 wget https://dl.fedoraproject.org/pub/fedora/linux/releases/38/Everything/x86_64/os/Packages/g/gnome-shell-extension-window-list-44.0-1.fc38.noarch.rpm
@@ -14,8 +9,6 @@ cd ./gnome-shell-extension-window-list
 rpm2cpio ../gnome-shell-extension-window-list-44.0-1.fc38.noarch.rpm | cpio -idmv
 
 # Get build deps
-ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
-DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata
 apt-get build-dep ./ -y
 
 # Build package
